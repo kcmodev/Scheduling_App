374ebf50b7dc5aee4a558c78a22212c92b0db73f
‘kcmodev’ <shchristensen3@gmail.com>
2020-08-04 22:46:29 -0500

    added add customer screed input validation

--- src/controller/AddCustomerController.java
+++ src/controller/AddCustomerController.java
@@ -4,13 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
-
 public class AddCustomerController {
     WindowManager window = new WindowManager();
 
     @FXML private TextField name;
     @FXML private TextField address;
     @FXML private TextField city;
+    @FXML private TextField state;
     @FXML private TextField zip;
     @FXML private TextField country;
     @FXML private TextField phone;
@@ -20,26 +20,29 @@ public class AddCustomerController {
         String name = this.name.getText();
         String address = this.address.getText();
         String city = this.city.getText();
+        String state = this.state.getText();
         String zip = this.zip.getText();
         String country = this.country.getText();
         String phone = this.phone.getText();
 
 
-        if (!isValidInput(name) || name.isEmpty()) {
+        if (!isValidInput(name)) {
             PopupHandlers.errorAlert(2, "Invalid name");
         }else if (!address.matches("^[0-9a-zA-Z_ ]*$") || address.isEmpty()){
             PopupHandlers.errorAlert(2, "Invalid address");
-        }else if (!isValidInput(city) || city.isEmpty()) {
+        }else if (!isValidInput(city)) {
             PopupHandlers.errorAlert(2, "Invalid city");
+        }else if (!isValidInput(state)){
+            PopupHandlers.errorAlert(2, "Invalid state");
         }else if (!zip.matches("^[0-9]*$") || zip.isEmpty()) {
             PopupHandlers.errorAlert(2, "Invalid zip");
-        }else if (zip.length() > 5){
+        }else if (zip.length() > 5 || zip.isEmpty()){
             PopupHandlers.errorAlert(2, "Valid zip code is 5 digits or less");
-        }else if (!isValidInput(country) || country.isEmpty()) {
+        }else if (!isValidInput(country)) {
             PopupHandlers.errorAlert(2, "Invalid country");
         }else if (!phone.matches("^[0-9]*$") || phone.isEmpty()) {
             PopupHandlers.errorAlert(2, "Invalid phone number. Numbers only");
-        } else if (phone.length() != 10){
+        } else if (phone.length() != 10 || phone.isEmpty()){
             PopupHandlers.errorAlert(2, "Phone number must be 10 digits. Numbers only.");
         }
         System.out.println("input successfully validated");
@@ -52,9 +55,7 @@ public class AddCustomerController {
     }
 
     public boolean isValidInput(String input){
-        if (input.matches("^[a-zA-Z0-9 _]*$") && !input.equals(null)){
-            return true;
-        }
+        if (input.matches("^[a-zA-Z0-9 _]*$") && !input.equals(null)){ return true; }
         return false;
     }
 }
